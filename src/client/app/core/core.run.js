9224d44b9a0216f1d0ce0b79d5ab9dd6e77017c2
ssbrewster <ssbrewster@gmail.com>
2015-12-09 21:58:28 +0000

    Debug ui-router
    Add event handles for $rootscope.$on to debug ui-router

--- src/client/app/core/core.run.js
+++ src/client/app/core/core.run.js
@@ -13,6 +13,25 @@
                 if (toState && toState.params && toState.params.autoActivateChild) {
                     $state.go(toState.params.autoActivateChild);
                 }
+            console.log('$stateChangeSuccess to '+toState.name+'- fired once the state transition is complete.');
+        });
+
+        $rootScope.$on('$stateChangeStart',function(event, toState, toParams, fromState, fromParams){
+          console.log('$stateChangeStart to '+toState.to+'- fired when the transition begins. toState,toParams : \n',toState, toParams);
+        });
+
+        $rootScope.$on('$stateChangeError',function(event, toState, toParams, fromState, fromParams){
+          console.log('$stateChangeError - fired when an error occurs during transition.');
+          console.log(arguments);
+        });
+
+        $rootScope.$on('$viewContentLoaded',function(event){
+          console.log('$viewContentLoaded - fired after dom rendered',event);
+        });
+
+        $rootScope.$on('$stateNotFound',function(event, unfoundState, fromState, fromParams){
+          console.log('$stateNotFound '+unfoundState.to+'  - fired when a state cannot be found by its name.');
+          console.log(unfoundState, fromState, fromParams);
         });
     }
 })();
